1bdc5d137596e7f0818455e108d319c4fe69e438
Victoria Sales <contatovictoriasales@gmail.com>
2023-01-27 16:34:24 -0300

    Adiciona as props ao formulário (Req.02)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled, onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <form>
         <fieldset>
@@ -15,6 +30,8 @@ class Form extends React.Component {
               type="text"
               name="name-input"
               data-testid="name-input"
+              value={ cardName }
+              onChange={ onInputChange }
               maxLength="20"
               required
             />
@@ -27,6 +44,8 @@ class Form extends React.Component {
               type="textarea"
               name="description-input"
               data-testid="description-input"
+              value={ cardDescription }
+              onChange={ onInputChange }
               maxLength="80"
               required
               rows="5"
@@ -41,6 +60,8 @@ class Form extends React.Component {
               type="number"
               name="attr1-input"
               data-testid="attr1-input"
+              value={ cardAttr1 }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -51,6 +72,8 @@ class Form extends React.Component {
               type="number"
               name="attr2-input"
               data-testid="attr2-input"
+              value={ cardAttr2 }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -61,6 +84,8 @@ class Form extends React.Component {
               type="number"
               name="attr3-input"
               data-testid="attr3-input"
+              value={ cardAttr3 }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -71,12 +96,20 @@ class Form extends React.Component {
               type="text"
               name="image-input"
               data-testid="image-input"
+              value={ cardImage }
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="rare-input">
             Raridade:
-            <select name="rare-input" id="rare-input" data-testid="rare-input">
+            <select
+              name="rare-input"
+              id="rare-input"
+              data-testid="rare-input"
+              value={ cardRare }
+              onChange={ onInputChange }
+            >
               <option value="normal">normal</option>
               <option value="raro">raro</option>
               <option value="muito raro">muito raro</option>
@@ -90,15 +123,38 @@ class Form extends React.Component {
               type="checkbox"
               name="trunfo-input"
               data-testid="trunfo-input"
+              checked={ cardTrunfo }
+              onChange={ onInputChange }
             />
           </label>
 
-          <button data-testid="save-button">Salvar</button>
+          <button
+            data-testid="save-button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
+          >
+            Salvar
+          </button>
 
         </fieldset>
       </form>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
